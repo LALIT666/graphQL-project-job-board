cd9b334c981916a2fc40347cfa9f0ba9b3b0377b
LALIT666 <89864324+LALIT666@users.noreply.github.com>
2026-04-12 15:15:48 +0200

    fetching jobs from database

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
-import JobList from '../components/JobList';
-import { jobs } from '../lib/fake-data';
+import JobList from "../components/JobList";
+import { jobs } from "../lib/fake-data";
+import { getJobs } from "../lib/graphql/queries.js‎";
+
+getJobs().then((jobs) => console.log("fetched jobs: ", jobs));
 
 function HomePage() {
   return (
     <div>
-      <h1 className="title">
-        Job Board
-      </h1>
+      <h1 className="title">Job Board</h1>
       <JobList jobs={jobs} />
     </div>
   );
