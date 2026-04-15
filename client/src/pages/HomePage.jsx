b46c12e062b6f148ec97c953eb85baa5a208476f
LALIT666 <89864324+LALIT666@users.noreply.github.com>
2026-04-15 15:57:46 +0200

    Basic Pagination UI

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,15 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import JobList from "../components/JobList";
 import { useJobs } from "../lib/graphql/hooks";
 
+const JOBS_PER_PAGE = 5;
+
 function HomePage() {
-  const { jobs, loading, error } = useJobs();
+  const [currentPage, setCurrentPage] = useState(1);
+  const { jobs, loading, error } = useJobs(
+    JOBS_PER_PAGE,
+    (currentPage - 1) * JOBS_PER_PAGE,
+  );
 
   console.log("[homePage] {Jobs, loading, error}:", { jobs, loading, error });
   if (loading) {
@@ -17,6 +23,14 @@ function HomePage() {
   console.log("[HomePage] jobs:", jobs);
   return (
     <div>
+      <div>
+        <button onClick={() => setCurrentPage(currentPage - 1)}>
+          Previous
+        </button>
+        <span>{currentPage}</span>
+
+        <button onClick={() => setCurrentPage(currentPage + 1)}>Next</button>
+      </div>
       <h1 className="title">Job Board</h1>
       <JobList jobs={jobs} />
     </div>
