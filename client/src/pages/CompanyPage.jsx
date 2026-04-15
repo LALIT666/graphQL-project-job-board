825f7aaf9ae957acc4c724e7bb87168c6a3c36da
LALIT666 <89864324+LALIT666@users.noreply.github.com>
2026-04-15 07:24:16 +0200

    custom hook useCompany

--- client/src/pages/CompanyPage.jsx
+++ client/src/pages/CompanyPage.jsx
@@ -1,15 +1,12 @@
 import { useParams } from "react-router";
 import JobList from "../components/JobList";
-import { useQuery } from "@apollo/client";
-import { companyByIdQuery } from "../lib/graphql/queries";
+import { useCompany } from "../lib/graphql/hooks";
 
 function CompanyPage() {
   const { companyId } = useParams();
-  const { data, loading, error } = useQuery(companyByIdQuery, {
-    variables: { id: companyId },
-  });
+  const { company, loading, error } = useCompany(companyId);
 
-  console.log("[CompanyPage]", { data, loading, error });
+  console.log("[CompanyPage]", { company, loading, error });
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -18,7 +15,6 @@ function CompanyPage() {
     return <div className="has-text-danger">Data unavailable</div>;
   }
 
-  const { company } = data;
   return (
     <div>
       <h1 className="title"> {company.name}</h1>
